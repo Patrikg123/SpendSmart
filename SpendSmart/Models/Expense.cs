bc4b68a7cdc44101ec0ef9e902e7e7e7d85e5df7
Patrikg123 <patrikg123@hotmail.com>
2025-11-11 14:39:27 +0100

    Test2

--- SpendSmart/Models/Expense.cs
+++ SpendSmart/Models/Expense.cs
@@ -9,6 +9,6 @@ namespace SpendSmart.Models
         public decimal Value { get; set; }
 
         [Required   ]
-        public string? Description { get; set; } 
+        public string? Description { get; set; } //test2
     }
 }
