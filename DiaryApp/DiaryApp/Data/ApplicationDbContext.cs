c23f45aa1b36fe3232e7f4af0d771fbfd90047d3
Patrikg123 <patrikg123@hotmail.com>
2025-12-02 19:24:19 +0100

    Seeded the database with data

--- DiaryApp/DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/DiaryApp/Data/ApplicationDbContext.cs
@@ -10,6 +10,18 @@ namespace DiaryApp.Data
         {
         }
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry { Id = 1, Title = "Went Hiking", Content = "Went hiking with Joe", Created = DateTime.Now },
+
+                new DiaryEntry { Id = 2, Title = "Went shopping", Content = "Went shopping with Joe", Created = DateTime.Now },
+                new DiaryEntry { Id = 3, Title = "Went to the movies", Content = "Went to the movies with Joe", Created = DateTime.Now }
+                );
+                }
     }
     
 }
---
+++ DiaryApp/DiaryApp/Migrations/20251202181649_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe", new DateTime(2025, 12, 2, 19, 16, 48, 48, DateTimeKind.Local).AddTicks(7552), "Went Hiking" },
+                    { 2, "Went shopping with Joe", new DateTime(2025, 12, 2, 19, 16, 48, 48, DateTimeKind.Local).AddTicks(7613), "Went shopping" },
+                    { 3, "Went to the movies with Joe", new DateTime(2025, 12, 2, 19, 16, 48, 48, DateTimeKind.Local).AddTicks(7614), "Went to the movies" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
